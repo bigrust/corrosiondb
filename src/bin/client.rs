f0425223ae03c49c33dee4e7f53a3bbb96fa2047
Guillem Nieto <gnieto.talo@gmail.com>
2018-06-24 22:56:04 +0200

    Add rustyline to have a little REPL

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,8 +1,11 @@
 extern crate corrosiondb;
 extern crate grpc;
 extern crate futures;
+extern crate rustyline;
 
 use grpc::Client;
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
 
 use corrosiondb::io::{
     coordinator::*,
@@ -10,13 +13,61 @@ use corrosiondb::io::{
 };
 
 fn main() {
-    let grpc_client = Client::new_plain("::1", 24322, Default::default()).unwrap();
-    let client = CoordinatorClient::with_client(grpc_client);
+    let mut rl = Editor::<()>::new();
 
-    let mut req = PingRequest::new();
-    req.set_payload("ping".to_string());
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                let cmd = parse_command(line).unwrap();
+                let result = handle_command(cmd);
 
-    let resp = client.ping(grpc::RequestOptions::new(), req);
-    let ping_reply = resp.wait().unwrap().1;
-    println!("{:?}", ping_reply);
+                match result {
+                    Ok(ok) => {
+                        println!("{}", ok)
+                    },
+                    Err(err) => {
+                        println!("ERROR:{}", err)
+                    }
+                }
+            },
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break
+            },
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break
+            },
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break
+            }
+        }
+    }
+}
+
+enum Command {
+    Ping,
+}
+
+fn parse_command(_command: String) -> Result<Command, ()> {
+    Ok(Command::Ping)
+}
+
+fn handle_command(cmd: Command) -> Result<String, String> {
+    match cmd {
+        Command::Ping => {
+            let grpc_client = Client::new_plain("::1", 24322, Default::default()).unwrap();
+            let client = CoordinatorClient::with_client(grpc_client);
+
+            let mut req = PingRequest::new();
+            req.set_payload("ping".to_string());
+
+            let resp = client.ping(grpc::RequestOptions::new(), req);
+            let ping_reply = resp.wait().map_err(|_| "grpc error".to_string()) ?.1;
+
+            Ok(ping_reply.get_payload().to_string())
+        }
+    }
 }
