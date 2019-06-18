92610894d8e0bdedcf373b7763ad7a69c76555b2
Alberto Fernández <albertofem@gmail.com>
2019-06-18 23:20:58 +0200

    Fix

--- ci/install-protobuf.sh
+++ ci/install-protobuf.sh
@@ -3,11 +3,11 @@
 set -x
 
 cd
-mkdir -p local
+mkdir -p $HOME/local
 
 PROTOC_URL=https://github.com/protocolbuffers/protobuf/releases/download/v3.8.0/protoc-3.8.0-linux-x86_64.zip
 
 curl -fSsL ${PROTOC_URL} -o protoc.zip
-unzip protoc.zip -d local
+unzip protoc.zip -d $HOME/local
 
-find local
+find $HOME/local
